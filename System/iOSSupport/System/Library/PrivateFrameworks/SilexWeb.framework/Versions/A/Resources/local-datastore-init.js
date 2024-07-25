cbd336443943674decd5255002c02b1991acca51
curiousandglobal <brorsenhj@gmail.com>
2024-07-25 16:10:08 -0400

    Restructured event_creation_branch for merge

---
+++ System/iOSSupport/System/Library/PrivateFrameworks/SilexWeb.framework/Versions/A/Resources/local-datastore-init.js
@@ -0,0 +1,11 @@
+/// Wrap everything in a function to prevent globally exposing temporary state
+(function() {
+    /// Consume all keys on init
+    let datastore = %@;
+
+    /// Enumerate the new values and add them to the local datastore without triggering messages
+    for (let [key, value] of Object.entries(datastore)) {
+        applenews.localDatastore[key] = value;
+    }
+})()
+
